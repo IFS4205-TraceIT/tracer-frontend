ae8b16a5095e0e02d079224db62afd1c023dfd9e
Kelvin Neo <2332196+kelvneo@users.noreply.github.com>
2022-09-17 23:36:32 +0800

    Add logout feature

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,4 +1,3 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
 import { LoginResponse } from "~/types/AuthUser";
 
@@ -72,6 +71,7 @@ async function onSubmit() {
           name="username"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           required
+          autofocus
         />
       </div>
       <div>
